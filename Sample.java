4648b5ffc991d957b970e190214e9144068c2676
hem <hemsainath15@gmail.com>
2023-05-06 14:25:46 +0530

    corrected

--- Sample.java
+++ Sample.java
@@ -1,7 +1,7 @@
 Class Sample
 {
-public static void main(String args[])
-{
-  System.out.println("Hello World")
-}
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
 }
