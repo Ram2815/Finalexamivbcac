438cf5677d6a3a5fdd5600e4d13cff30c371c954
afshan-codes <afshanbarlaskar07@gmail.com>
2023-05-06 11:49:59 +0530

    merge

--- exam.java
+++ exam.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
  public class DateToStringExample1 {  
        public static void main(String args[]){  
                 Date date = Calendar.getInstance().getTime();
-        a=1
+        a=4
          
                 DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
                 String strDate = dateFormat.format(date);  
